d28f75d23e48f3300f3a5fe50f52b4aa300ce28a
elecfrog <1072081744@qq.com>
2025-03-08 23:12:27 +0800

    Resolve SPA-10001: [Engine Refactor] Reorganize code

--- build_cmake/external/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Sparrow-Renderer/external/assimp/contrib/zlib
-# Build directory: D:/Sparrow-Renderer/build_cmake/external/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
