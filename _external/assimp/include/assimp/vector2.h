8341591eb32e0e757ca4cc3468d75d47bf781147
elecfrog <1072081744@qq.com>
2025-03-08 23:13:04 +0800

    Resolve SPA-10001: [Engine Refactor] Reorganize code

--- _external/assimp/include/assimp/vector2.h
+++
@@ -1,9 +0,0 @@
-/** @file vector2.h
- */
-#pragma once
-#include "defs.h"
-#include <../../glm/glm/glm.hpp>
-
-//template<typename T, glm::qualifier>
-//using aiVector2t = glm::vec<2, T, glm::defaultp>;
-using aiVector2D = glm::vec2;
